5bc4e6dd6e9af0649415437a45641dc7811f5058
Glen Selle <glenselle@me.com>
2020-04-16 18:17:05 -0500

    Added upload endpoint

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +22,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.post('/upload', upload.single('soundfile'), function (req, res, next) {
+  // req.file
+  console.log('do something with the upload')
+  return res.sendStatus(200)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
