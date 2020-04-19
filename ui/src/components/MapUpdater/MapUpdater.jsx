54acb1eb09a2716707bd978dcc50705b385fa942
Noah Tatko <tatkon@rbtrainers.com>
2020-04-18 23:39:52 -0500

    this is very annoying, but now it works.

--- ui/src/components/MapUpdater/MapUpdater.jsx
+++ ui/src/components/MapUpdater/MapUpdater.jsx
@@ -38,13 +38,25 @@ class MapUpdater extends Component {
   async accidents() {
     const { map } = this.props;
     console.log("original bounds", map.getView().calculateExtent(map.getSize()))
-    const bounds = proj.transformExtent(map.getView().calculateExtent(map.getSize()), "EPSG:3857", "EPSG:4326")
+    const bounds = proj.transformExtent(map.getView().calculateExtent(map.getSize()), 'EPSG:3857', 'EPSG:4326')
     console.log("bounds", bounds)
+    while (-180 > bounds[0] || bounds[0] > 180) {
+      console.log("infinite loop?")
+      if (bounds[0] < -180) {
+        bounds[0] = bounds[0] + 360;
+        bounds[2] = bounds[2] + 360;
+      } else if (bounds[0] > 180) {
+        bounds[0] = bounds[0] - 360;
+        bounds[2] = bounds[2] - 360
+      }
+    }
+    const transformedBounds = [bounds[1], bounds[0], bounds[3], bounds[2]]
+    console.log("bounds", transformedBounds)
     if (this.state.accidentExtent && extent.containsExtent(this.state.accidentExtent, map.getView().calculateExtent(map.getSize()))) {
       // if a previous extent contains the current extent, do nothing
       return;
     }
-    if (bounds.length !== 4 || Math.abs(bounds[0]-bounds[2]) > 0.2 || Math.abs(bounds[1]-bounds[3]) > 0.2) {
+    if (bounds.length !== 4 || Math.abs(transformedBounds[0]-transformedBounds[2]) > 0.7 || Math.abs(transformedBounds[1]-transformedBounds[3]) > 0.7) {
       console.log("too far away to request anything")
       // if we have the wrong bounds, or the extent is too large, do nothing
       // mapquest does not allow extends broader than .2 of lat or long
@@ -52,8 +64,8 @@ class MapUpdater extends Component {
     }
     //build mapquest query
     const key = '82OD8xUAEGtjlGG8QmixjVe90rErA3NU';
-    const coordArg = bounds.join('%2C');
-    const url = `http://www.mapquestapi.com/traffic/v2/incidents?key=${ key }&boundingBox=${ coordArg }&filters=incidents`
+    const coordArg = transformedBounds.join('%2C');
+    const url = `http://www.mapquestapi.com/traffic/v2/incidents?key=${ key }&boundingBox=${ coordArg }&filters=incidents&outFormat=json`
     
     // save extent to state
     this.setState({accidentExtent: map.getView().calculateExtent(map.getSize())})
@@ -61,7 +73,13 @@ class MapUpdater extends Component {
     // get mapquest incidents
     console.log("url", url)
     const response = await fetch(url)
-    console.log("response", response)
+    if (response.status !== 200) {
+      throw Error(response.message);
+    }
+    const json = await response.json()
+    json.incidents && json.incidents.forEach((incident) => {
+      this.addNewPoint({event: 'traffic incident', coords: { lat: incident.lat, long: incident.lng }, text: incident.fullDesc})
+    })
   }
 
   render() {
